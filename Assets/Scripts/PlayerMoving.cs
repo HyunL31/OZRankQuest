a842db1950746203320a821ca72293ff4f44047b
이주현 <juhyun040100@gmail.com>
2026-04-25 17:53:13 +0900

    [Feat] 승급과제 - 플래티넘

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMoving : MonoBehaviour
 {
+    PlayerState playerState = new PlayerState();
+
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
     public float rotateSpeed = 10f;
@@ -9,14 +12,14 @@ public class PlayerMoving : MonoBehaviour
     public float doubleJumpPower = 3f;
     public AudioSource walkSound;
     public AudioSource runSound;
+    public AudioClip jumpSound;
+    public AudioClip doubleJump;
+
+    public Image staminaGage;
 
     private Rigidbody _rb;
     private Animator _anim;
 
-    private int stamina = 100;
-    private bool isGround = true;
-    private bool isRunning = false;
-    private bool isWalking = false;
     private int jumpCount = 0;
 
     void Start()
@@ -35,20 +38,23 @@ public class PlayerMoving : MonoBehaviour
             Jump();
         }
 
-        if (isGround)
+        if (playerState.isGround)
         {
             Move(inputX, inputZ);
         }
 
+        playerState.Stamina();
         AnimControl();
+
+        CheckStamina();
     }
 
     private void Move(float inputX, float inputZ)
     {
-        if (inputX == 0 && inputZ == 0)
+        if ((inputX == 0 && inputZ == 0) || playerState.stamina <= 0)
         {
-            isWalking = false;
-            isRunning = false;
+            playerState.isWalking = false;
+            playerState.isRunning = false;
 
             runSound.Pause();
             walkSound.Pause();
@@ -57,11 +63,11 @@ public class PlayerMoving : MonoBehaviour
         }
 
         float moveSpeed = walkSpeed;
-        isWalking = true;
-        isRunning = false;
+        playerState.isWalking = true;
+        playerState.isRunning = false;
 
         runSound.Pause();
-        
+
         if (!walkSound.isPlaying)
         {
             walkSound.Play();
@@ -69,12 +75,16 @@ public class PlayerMoving : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            isRunning = true;
-            isWalking = false;
+            playerState.isRunning = true;
+            playerState.isWalking = false;
             moveSpeed = runSpeed;
 
             walkSound.Pause();
-            runSound.Play();
+
+            if (!runSound.isPlaying)
+            {
+                runSound.Play();
+            }
         }
 
         _rb.linearVelocity = new Vector3(inputX, 0, inputZ) * moveSpeed;
@@ -87,15 +97,17 @@ public class PlayerMoving : MonoBehaviour
 
     private void Jump()
     {
-        if (jumpCount >= 2)
+        if (jumpCount >= 2 || playerState.stamina <= 8)
         {
             return;
         }
 
-        isGround = false;
+        playerState.isGround = false;
         jumpCount++;
 
-        stamina -= 10;
+        walkSound.Pause();
+        runSound.Pause();
+        walkSound.PlayOneShot(jumpSound);
 
         float power = jumpCount == 1 ? jumpPower : doubleJumpPower;
 
@@ -103,24 +115,70 @@ public class PlayerMoving : MonoBehaviour
 
         if (jumpCount == 2)
         {
-            stamina -= 5;
+            playerState.stamina -= 5;
             _anim.SetTrigger("DoubleJump");
+            walkSound.PlayOneShot(doubleJump);
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGround = true;
+            playerState.isGround = true;
             jumpCount = 0;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            playerState.isGround = false;
         }
     }
 
     private void AnimControl()
     {
-        _anim.SetBool("isJumping", !isGround);
-        _anim.SetBool("isWalking", isGround && isWalking);
-        _anim.SetBool("isRunning", isGround && isRunning);
+        _anim.SetBool("isJumping", !playerState.isGround);
+        _anim.SetBool("isWalking", playerState.isGround && playerState.isWalking);
+        _anim.SetBool("isRunning", playerState.isGround && playerState.isRunning);
+    }
+
+    private void Die()
+    {
+        _anim.SetTrigger("isDie");
+    }
+
+    private void CheckStamina()
+    {
+        playerState.ChangeState();
+
+        staminaGage.fillAmount = playerState.stamina / 100f;
+
+        Color gageColor = Color.white;
+
+        if (playerState.state == State.Good)
+        {
+            gageColor = Color.green;
+        }
+        else if (playerState.state == State.Normal)
+        {
+            gageColor = Color.yellow;
+        }
+        else if (playerState.state == State.Bad)
+        {
+            gageColor = Color.red;
+        }
+
+        staminaGage.color = gageColor;
+
+        if (playerState.stamina <= 0)
+        {
+            playerState.isWalking = false;
+            playerState.isRunning = false;
+
+            Die();
+        }
     }
 }
---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public enum State
+{
+    Good,
+    Normal,
+    Bad
+}
+
+public class PlayerState
+{
+    public State state = State.Good;
+    public float regenStamina = 1f;
+    public float stamina = 100f;
+    public float maxStamina = 100f;
+    public bool isGround = true;
+    public bool isRunning = false;
+    public bool isWalking = false;
+
+    public void Stamina()
+    {
+        if (isWalking)
+        {
+            stamina -= 1 * Time.deltaTime;
+        }
+        else if (isRunning)
+        {
+            stamina -= 3 * Time.deltaTime;
+        }
+        else if (!isGround)
+        {
+            stamina -= 10 * Time.deltaTime;
+        }
+        else if (stamina < maxStamina && stamina >= 0)
+        {
+            stamina += regenStamina * Time.deltaTime;
+        }
+    }
+
+    public void ChangeState()
+    {
+        if (stamina >= 60)
+        {
+            state = State.Good;
+        }
+        else if (stamina >= 30)
+        {
+            state = State.Normal;
+        }
+        else
+        {
+            state = State.Bad;
+        }
+    }
+}
